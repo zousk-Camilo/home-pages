5047d71c7c242830ff645536baff9afe3bd908df
zousk-Camilo <112782949+zousk-Camilo@users.noreply.github.com>
2023-02-22 21:46:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//variables del DOM
+
+const menuIconBtn = document.querySelector(".header__container-image__menu");
+const menuCloseIconBtn = document.querySelector(".modal-navbar__close-icon");
+const navbar = document.querySelector(".modal-navbar")
+const body = document.getElementsByTagName("body"); 
+
+console.log(navbar)
+
+menuIconBtn.addEventListener("click", ()=>{
+
+     navbar.style.display = "block";
+
+})
+
+
+menuCloseIconBtn.addEventListener("click", ()=>{
+
+    navbar.style.display = "none"
+
+})
+
+
+
+
